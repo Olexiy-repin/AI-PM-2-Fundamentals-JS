d6d3693238809c8606adfa6cda9f342ec87c2c35
Olexiy-repin <repinlyoha@gmail.com>
2026-06-03 21:33:37 +0300

    classwork

---
+++ js/01-vars.js
@@ -0,0 +1,36 @@
+/*
+ * Змінні та типи даних
+ *
+ * - Оголошення змінних з let та const
+ * - Найменування змінних
+ * - Перевизначення значення
+ * - Вибір між const і let
+ * - Виведення даних
+ * - Отримання даних
+ * - Примітивні типи даних: number, string, boolean, null, undefined.
+ * - Оператор typeof
+ */
+
+// const pricePerItem = 1000;
+
+// console.log(pricePerItem);
+
+// pricePerItem = 800;
+
+// console.log(price);
+
+// alert('Hello user!');
+
+// const firstName = prompt('Enter your first name!');
+
+// console.log(firstName);
+
+// const num = 100;
+
+// console.log(num);
+// console.log(typeof num);
+
+// const str = 'Hello world!';
+
+// console.log(str);
+// console.log(typeof str);
