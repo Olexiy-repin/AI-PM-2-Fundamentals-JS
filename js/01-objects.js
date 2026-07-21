4bdb129fa2e0a163590d15418703fd89a3b0188b
Olexiy-repin <repinlyoha@gmail.com>
2026-07-21 21:06:13 +0300

    classwork

---
+++ js/01-objects.js
@@ -0,0 +1,65 @@
+/*
+ * Об'єкти
+ *
+ * - Створення об'єкта
+ * - Доступ до властивостей через крапку
+ * - Доступ до властивостей через квадратні дужки
+ * - Вкладені властивості
+ * - Доступ до вкладених властивостей
+ * - Зміна значення властивостей
+ * - Додавання властивостей
+ */
+
+// const courseHTMLTitle = 'HTML Fundamentals';
+// const courseHTMLDescription = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, nam.';
+// const courseHTMLRating = 9;
+
+// const courseHTMLArr = [
+//   'HTML Fundamentals',
+//   'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, nam.',
+//   9,
+//   10000,
+// ];
+
+// const courseHTML = {
+//   title: 'HTML Fundamentals',
+//   description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, nam.',
+//   rating: 9,
+//   teachers: ['Maurice Wade', 'Elizabeth Shelton', 'Louis Evans'],
+//   images: {
+//     l: 'https://my-site/course-html-l.jpg',
+//     m: 'https://my-site/course-html-m.jpg',
+//     s: 'https://my-site/course-html-s.jpg',
+//   },
+// };
+
+// console.log(courseHTML);
+
+// console.log(courseHTML.images.s);
+
+// console.log(courseHTML.teachers[0]);
+
+// courseHTML.teachers.push('Don Castillo');
+
+// console.log(courseHTML);
+
+// courseHTML.price = 10000;
+// courseHTML.rating = 10;
+
+// console.log(courseHTML);
+
+// console.log(courseHTML.title);
+// console.log(courseHTML.rating);
+
+// console.log(courseHTML['price']);
+// console.log(courseHTML['description']);
+
+// console.log(courseHTML['ti' + 'tle']);
+
+// const key = 'price';
+// console.log(courseHTML[key]);
+
+// const getKey = function () {
+//   return 'rating';
+// };
+// console.log(courseHTML[getKey()]);
---
+++ js/02-refs.js
@@ -0,0 +1,19 @@
+/*
+ * Об'єкти
+ *
+ * - Тип даних за посиланням
+ */
+
+// const user = {
+//   firstName: 'Maude',
+//   lastName: 'Norman',
+// };
+// const userCopy = user;
+
+// console.log('user =', user);
+// console.log('userCopy =', userCopy);
+
+// userCopy.firstName = 'Devin';
+
+// console.log('user =', user);
+// console.log('userCopy =', userCopy);
