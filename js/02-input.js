a115eef34cebd5361b539febdf8c0acc53e180e4
Olexiy-repin <repinlyoha@gmail.com>
2026-08-13 21:11:58 +0300

    classwork

--- js/02-input.js
+++ js/02-input.js
@@ -4,15 +4,39 @@
  * - Подія change
  */
 
+const refs = {
+  textField: document.querySelector('.js-user-name'),
+  policyCheckbox: document.querySelector('.js-policy-checkbox'),
+};
+
 /*
 TODO: Виводь в консоль все що користувач вводить в input
 */
+// refs.textField.addEventListener('input', () => {
+//   console.log(refs.textField.value);
+// });
 
 /*
 TODO: Користувач вводить в input своє ім'я, після втрати
 TODO: фокусу отримує alert з повідомленням-привітанням
  */
+// refs.textField.addEventListener('blur', () => {
+//   if (refs.textField.value.length >= 2) {
+//     alert(`Welcome ${refs.textField.value}`);
+//   }
+// });
+
+// refs.textField.addEventListener('change', () => {
+//   if (refs.textField.value.length >= 2) {
+//     alert(`Welcome ${refs.textField.value}`);
+//   }
+// });
 
 /*
 TODO: Після того, як користувач активує чекбокс, виводиться alert
 */
+// refs.policyCheckbox.addEventListener('change', () => {
+//   if (refs.policyCheckbox.checked) {
+//     alert('All done!');
+//   }
+// });
--- js/03-forms.js
+++ js/03-forms.js
@@ -7,3 +7,28 @@
 /*
 TODO: Оброби форму та збережи відгук користувача в об'єкт
 */
+const refs = {
+  feedbackForm: document.querySelector('.js-feedback-form'),
+};
+
+const onFeedbackFormSubmit = event => {
+  event.preventDefault();
+
+  const formData = {
+    [refs.feedbackForm.elements.username.name]: refs.feedbackForm.elements.username.value.trim(),
+    [refs.feedbackForm.elements.email.name]: refs.feedbackForm.elements.email.value.trim(),
+    [refs.feedbackForm.elements.comment.name]: refs.feedbackForm.elements.comment.value.trim(),
+  };
+
+  const formDataValues = Object.values(formData);
+
+  if (formDataValues.includes('')) {
+    alert('Всі поля мають бути заповнені!');
+
+    return;
+  }
+
+  console.log(formData);
+};
+
+refs.feedbackForm.addEventListener('submit', onFeedbackFormSubmit);
